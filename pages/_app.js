012a8d8e578de5b81ad7aa086712f24ebd3cd070
Trent Farley <trent.a.farley@gmail.com>
2021-06-15 17:18:41 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+import '/node_modules/bootstrap/dist/css/bootstrap.css'
+function Puma({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Puma
