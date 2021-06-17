8c3e0cbc1443f8d8a88c2f551baa3a23786af76e
Trent Farley <trent.a.farley@gmail.com>
2021-06-17 13:53:38 -0700

    update to config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://projectpuma.netlify.app';
+export const server = dev ? 'http://localhost:3000' : 'https://projectpuma.netlify.app/.netlify/functions/next_api_get_listings';
