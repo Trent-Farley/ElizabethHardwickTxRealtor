ff5e568e366ecf7bc0713d4a03c1756e90a7410c
Trent Farley <trent.a.farley@gmail.com>
2021-07-19 18:42:49 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const get_listings = dev ? 'http://localhost:8000/api/get_listings' : 'https://www.elizabethhardwickrealitor.com/.netlify/functions/next_api_get_listings'
+export const get_listings = dev ? 'http://localhost:8000/api/get_listings' : 'https://www.elizabethhardwickrealtor.com/.netlify/functions/next_api_get_listings'
 export const server = dev ? 'http://localhost:3000' : 'https://projectpuma.netlify.app/.netlify/functions/next_api_get_listings';
