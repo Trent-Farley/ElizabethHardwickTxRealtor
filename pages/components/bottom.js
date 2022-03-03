f841824001ba410bbd8303d902611c0b0301e52b
Trent Farley <trent.a.farley@gmail.com>
2022-03-02 20:23:48 -0800

    Added gtag to every page

--- pages/components/bottom.js
+++ pages/components/bottom.js
@@ -1,9 +1,22 @@
+import Script from 'next/script'
+
 export default function Bottom(){
     return(
         <footer>
         <div className=" container card-footer text-muted sticky-bottom position-bottom w-100">
           Located at: <strong>RE/MAX Four Corners 7701 W. Eldorado Pkwy, #400 McKinney, TX 75070</strong>
         </div>
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {dataLayer.push(arguments);}
+            window.addEventListener('load', function(){
+              if(window.location.pathname == '/'){
+                gtag('event', 'conversion', {'send_to': 'AW-10821860041/Iq-pCKvH6YwDEMn1oago'});
+              }
+            });
+          `}
+        </Script>
       </footer>
     );
 }
--- pages/components/menu.js
+++ pages/components/menu.js
@@ -1,8 +1,46 @@
 import {Navbar, Nav} from 'react-bootstrap'
 import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
+
 export default function Menu(){
     return(
         <>
+         <Head>
+          <title>Home</title>
+          <meta name="description" content="Find your next home with Elizabeth Hardwick. Get information about current homes on the market as well as contact information to get in touch with Elizabeth. " />
+          <link rel="icon" href="/favicon.ico" />
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10821860041"  strategy="beforeInteractive"/>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10821860041" strategy="beforeInteractive"/>
+          <Script strategy="beforeInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10821860041');
+              gtag('config', 'AW-10821860041/QqIaCIj4ko0DEMn1oago', {
+                'phone_conversion_number': '(469) 850-2170'
+              });
+            `}
+          </Script>
+          <Script strategy='beforeInteractive'>
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10821860041');
+          `}
+          </Script>
+          <Script strategy='beforeInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('config', 'AW-10821860041/QqIaCIj4ko0DEMn1oago', {
+                'phone_conversion_number': '(469) 850-2170'
+              });
+            `}
+          </Script>
+        </Head>
           <Navbar variant='dark' className="pb-0 mb-0" bg='dark'>
             <Nav className="mr-auto">
               <Nav.Link >
